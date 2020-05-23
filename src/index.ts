b4e9ef10d68b42df2da0ebeee9cde37983d2a118
s63907 <alvaroramirezc@bcp.com.pe>
2020-05-23 14:50:53 -0500

    123

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 import { red } from 'turbocolor';
-import { exec } from 'child_process';
+import newSchema from './new/index';
 
 function run() {
   console.log(red('Alvar ramirez castillo 1s'));
-  const command = `schematics blank alvaro`
-  exec(command);
+  newSchema();
 }
 
 run();
--- src/new/index.ts
+++ src/new/index.ts
@@ -1,11 +1,43 @@
-import { chain, externalSchematic } from '@angular-devkit/schematics';
+import { externalSchematic, Rule, SchematicContext, Tree, mergeWith, apply, empty } from '@angular-devkit/schematics';
 
-export default function newSchema() {
-  return chain([
-    externalSchematic('@schematics/angular', 'ng-new', {
-      createApplication: false,
-      name: 'mark-1',
-      version: '9.1.7'
-    })
-  ]);
+export default function newSchema(): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    return mergeWith(
+      apply(empty(), [
+        externalSchematic('@schematics/angular', 'ng-new', {
+          createApplication: false,
+          name: 'mark-1',
+          version: '9.1.7'
+        })
+      ])
+    )(tree, _context);
+  }
+
+  // return chain([
+  //   externalSchematic('@schematics/angular', 'ng-new', {
+  //     createApplication: false,
+  //     name: 'mark-1',
+  //     version: '9.1.7'
+  //   })
+  // ]);
 }
+
+
+// export default function newBcp(_options: Schema): Rule {
+//   return (tree: Tree, _context: SchematicContext) => {
+//     const optionMonoRepo = 'Mono Repo';
+//     const optionMicroApps = 'Micro frontends';
+//     let runSchematics = '';
+//     if (_options.selected === optionMonoRepo) {
+//       runSchematics = 'mono-repo';
+//     }
+//     if (_options.selected === optionMicroApps) {
+//       runSchematics = 'micro-apps';
+//     }
+//     return mergeWith(
+//       apply(empty(), [
+//         schematic(runSchematics, {})
+//       ])
+//     )(tree, _context);
+//   }
+// }
