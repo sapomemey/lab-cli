ded85aa38b0d1f5279538a91066446bd72ae4d8b
s63907 <alvaroramirezc@bcp.com.pe>
2020-05-23 10:56:35 -0500

    3

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 import { red } from 'turbocolor';
-import newSchema from './new/index';
+import { exec } from 'child_process';
+import { resolve } from 'path';
 
 function run() {
   console.log(red('Alvar ramirez castillo'));
-  newSchema();
+  const command = `node ${resolve('node_modules/.bin/schematics blank --name=bvi')}`
+  exec(command);
 }
 
 run();
