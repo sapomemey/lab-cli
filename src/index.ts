c3c423ee2e6f786e47cea8762ca838ec178690c2
s63907 <alvaroramirezc@bcp.com.pe>
2020-05-23 14:27:12 -0500

    123

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 import { red } from 'turbocolor';
 import { exec } from 'child_process';
-import { resolve } from 'path';
 
 function run() {
   console.log(red('Alvar ramirez castillo 1s'));
-  const command = `${resolve('node_modules/.bin/schematics')} blank alvaro`
+  const command = `schematics blank alvaro`
   exec(command);
 }
 
