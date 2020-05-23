25d9cab6d40e9b026b1911aba4820276be6ece0b
s63907 <alvaroramirezc@bcp.com.pe>
2020-05-23 15:19:04 -0500

    123

--- src/new/index.ts
+++ src/new/index.ts
@@ -1,18 +1,7 @@
-import { externalSchematic, Rule, SchematicContext, Tree, mergeWith, apply, empty } from '@angular-devkit/schematics';
+// import { externalSchematic, Rule, chain } from '@angular-devkit/schematics';
 
-export default function newSchema(): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return mergeWith(
-      apply(empty(), [
-        externalSchematic('@schematics/angular', 'ng-new', {
-          createApplication: false,
-          name: 'mark-1',
-          version: '9.1.7'
-        })
-      ])
-    )(tree, _context);
-  }
-
+export default function newSchema() {
+  console.log('hola mundo');
   // return chain([
   //   externalSchematic('@schematics/angular', 'ng-new', {
   //     createApplication: false,
@@ -21,23 +10,3 @@ export default function newSchema(): Rule {
   //   })
   // ]);
 }
-
-
-// export default function newBcp(_options: Schema): Rule {
-//   return (tree: Tree, _context: SchematicContext) => {
-//     const optionMonoRepo = 'Mono Repo';
-//     const optionMicroApps = 'Micro frontends';
-//     let runSchematics = '';
-//     if (_options.selected === optionMonoRepo) {
-//       runSchematics = 'mono-repo';
-//     }
-//     if (_options.selected === optionMicroApps) {
-//       runSchematics = 'micro-apps';
-//     }
-//     return mergeWith(
-//       apply(empty(), [
-//         schematic(runSchematics, {})
-//       ])
-//     )(tree, _context);
-//   }
-// }
