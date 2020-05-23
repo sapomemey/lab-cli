12d73d2bc7a3e76efd36456e6628cf71501e31e8
s63907 <alvaroramirezc@bcp.com.pe>
2020-05-23 10:02:38 -0500

    123

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 import { blue } from 'turbocolor';
+import { exec } from 'child_process';
 
 function run() {
   console.log(blue('Alvar ramirez castillo'));
+
+  exec('schematics .:new');
 }
 
 run();
